160462ca03b2ca1294f72cd6667e37f18cd782eb
Awesome Hacker <awesome.hacker@test.com>
2022-02-03 18:42:10 -0800

    added iframe with Twitch Vide

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ namespace _425ShowWebsite.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public bool IsProd{ get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
