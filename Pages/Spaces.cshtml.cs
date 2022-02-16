b16ba56e2707b66197d86d0a54635eb502ef1ae7
Awesome Hacker <awesome.hacker@test.com>
2022-02-16 11:44:06 -0800

    Working solution

--- Pages/Spaces.cshtml.cs
+++ Pages/Spaces.cshtml.cs
@@ -5,15 +5,18 @@ namespace _425ShowWebsite.Pages;
 public class SpacesModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly TwitterSpaceService _twitterSpaceService;
 
+    public IEnumerable<TwitterSpace> TwitterSpaces { get; set; }
 
-    public SpacesModel(ILogger<IndexModel> logger)
+    public SpacesModel(ILogger<IndexModel> logger, TwitterSpaceService twitterSpaceService)
     {
         _logger = logger;
+        _twitterSpaceService = twitterSpaceService;
     }
 
-    public void OnGet()
+    public async Task OnGet()
     {
-        
+        TwitterSpaces = await _twitterSpaceService.GetData();
     }
 }
--- Services/TwitterSpaceService.cs
+++ Services/TwitterSpaceService.cs
@@ -1,31 +1,56 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 public class TwitterSpaceService
 {
-    private const string TwitterSpaceUrl = "";
+    private const string TwitterSpaceUrl = "https://raw.githubusercontent.com/425show/newWebsite/main/spaces.json";
     private HttpClient? _httpClient;
+    private IWebHostEnvironment _hostEnvironment;
 
-    public TwitterSpaceService(HttpClient httpClient)
+    public TwitterSpaceService(HttpClient httpClient, IWebHostEnvironment hostEnvironment)
     {
         _httpClient = httpClient;
+        _hostEnvironment = hostEnvironment;
     }
+
     public async Task<IEnumerable<TwitterSpace>> GetData()
-    {
-        var twitterSpaces = await _httpClient.GetFromJsonAsync<List<TwitterSpace>>(TwitterSpaceUrl);
+    {   
+        var rawJson = await File.ReadAllTextAsync(Path.Combine(_hostEnvironment.ContentRootPath, "spaces.json"));
+        var twitterSpaces = JsonSerializer.Deserialize<IEnumerable<TwitterSpace>>(rawJson);
         return twitterSpaces;
     }
 }
 public class Link
 {
+    [JsonPropertyName("linktext")]
     public string? LinkText { get; set; }
+    [JsonPropertyName("linkurl")]
     public string? LinkUrl { get; set; }
 }
 
 public class TwitterSpace
 {
+    [JsonPropertyName("topic")]
     public string? Topic { get; set; }
-    public DateTime? RecordingDate { get; set; }
-    public DateTime? ExpiryDate { get; set; }
+    [JsonPropertyName("recordingdate")]
+    public string? RecordingDate { get; set; }
+    [JsonPropertyName("expirydate")]
+    public string? ExpiryDate { get; set; }
+    [JsonPropertyName("link")]
     public string? Link { get; set; }
+    [JsonPropertyName("summary")]
     public string? Summary { get; set; }
+    [JsonPropertyName("links")]
     public List<Link>? Links { get; set; }
+
+    public DateTimeOffset StartDate{
+        get {
+            return DateTimeOffset.Parse(RecordingDate);
+        }
+    }
+
+    public DateTimeOffset EndDate{
+        get {
+            return DateTimeOffset.Parse(ExpiryDate);
+        }
+    }
 }
